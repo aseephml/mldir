14206e0921d62db0281407a82eb576b42a62b4fc
aseephml <45883788+aseephml@users.noreply.github.com>
2019-04-12 15:37:45 -0500

    Add files via upload

---
+++ framework/data_loader.py
@@ -0,0 +1,110 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+
+class DataLoader:
+    def __init__(self, path, *args, **kwargs):
+        self.generator = None
+        self.parameters = kwargs
+
+        self.data_path = path
+        self.train_path = os.path.join(self.data_path, 'train.csv')
+        self.test_path = os.path.join(self.data_path, 'test.csv')
+
+        # self._load_sample_data()
+        self._load_data()
+
+    def _load_data(self):
+        sys.stdout.write("Loading training data... ")
+        sys.stdout.flush()
+        self.train = pd.read_csv(self.train_path)
+        sys.stdout.write("Done\n")
+
+        sys.stdout.write("Loading test data... ")
+        sys.stdout.flush()
+        self.test = pd.read_csv(self.test_path)
+        sys.stdout.write("Done\n")
+
+        self._process_target_column()
+        self._process_id_column()
+
+    def _apply_preprocessors(self, method, **preprocessor_params):
+        processor = method(**preprocessor_params)
+        sys.stdout.write("Applying preprocessor {}... ".format(method.__name__))
+        sys.stdout.flush()
+        self.train = processor.fit_transform(self.train, self.train_y)
+        self.test = processor.transform(self.test)
+        sys.stdout.write("Done\n")
+
+    def _process_target_column(self):
+        if 'target' in self.parameters:
+            self.target_name = self.parameters['target']
+            self.train_y = self.train[self.target_name].values
+            self.train.drop(self.target_name, axis=1, inplace=True)
+
+    def _process_id_column(self):
+        if 'target' in self.parameters:
+            self.id_name = self.parameters['id']
+            self.train_idx = self.train[self.id_name].values
+            self.test_idx = self.test[self.id_name].values
+
+    def freeze_data(self):
+        self._frozen = (self.train, self.train_y, self.test,
+            self.train_idx, self.test_idx)
+
+    def generate_split(self, method, *args, **kwargs):
+        self.generator = method(*args, **kwargs)
+
+    def get_by_id(self, dtype, ids):
+        if dtype == 'train' and isinstance(self.train, np.ndarray):
+            return np.take(self.train, ids, axis=0), \
+                np.take(self.train_y, ids, axis=0)
+        elif dtype == 'train' and isinstance(self.train, pd.DataFrame):
+            return self.train.iloc[ids], np.take(self.train_y, ids, axis=0)
+        elif dtype == 'test' and isinstance(self.test, np.ndarray):
+            return np.take(self.test, ids, axis=0)
+        elif dtype == 'test' and isinstance(self.test, pd.DataFrame):
+            return self.test.iloc[ids]
+
+    def get_columns(self):
+        return self.train.columns
+
+    def get_parameter(self, parameter):
+        return self.parameters[parameter]
+
+    def get_split(self):
+        if self.generator == None:
+            raise Exception("Data generator is not set")
+
+        return self.generator.split(self.train, self.train_y)
+
+    def get_n_splits(self):
+        if self.generator == None:
+            raise Exception("Data generator is not set")
+
+        return self.generator.get_n_splits()
+
+    def get_target(self):
+        return self.train_y
+
+    def get_train(self):
+        return self.train, self.train_y, self.train_idx
+
+    def get_test(self):
+        return self.test, self.test_idx
+
+    def get_train_ids(self):
+        return self.train_idx
+
+    def get_test_ids(self):
+        return self.test_idx
+
+    def preprocess(self, *args, **preprocessor_params):
+        for method in args:
+            self._apply_preprocessors(method, **preprocessor_params)
+
+    def restore_frozen(self):
+        self.train, self.train_y, self.test, self.train_idx, self.test_idx = \
+self._frozen
---
+++ framework/model_loader.py
@@ -0,0 +1,151 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import datetime
+import shutil
+
+from .generic_model import GenericModel
+
+
+class ModelLoader:
+    def __init__(self, model, model_params, *args, **kwargs):
+        self.parameters = model_params
+        self.model = model(*args, **kwargs)
+        self._set_parameters()
+        self.online_preprocessors = {}
+
+    def _init_default_parameters(self):
+        self.model_name = 'defaultmodel'
+        self.fit_name = 'fit'
+        self.predict_name = 'predict'
+        self.preds_col_num = 0
+        self.online_val_func = None
+
+    def _apply_preprocessors(self, X, y, X_cv):
+        for method, params in self.online_preprocessors.items():
+            processor = method(**params)
+            X, y = processor.fit_transform(X, y)
+            X_cv = processor.transform(X_cv)
+
+        return X, y, X_cv
+
+    def _save_files(self, train, test, accuracy,
+          caller_path, preds_path, models_path):
+        model_name = self.get_parameter('name')
+        acc_str = str(float('%.4f' % accuracy)).split('.')[-1]
+        now = datetime.datetime.now()
+        dt_str = now.strftime("%d%m%H%M%S")
+
+        file_name = '_'.join([acc_str, model_name, dt_str])
+        test_path = os.path.join(preds_path, 'test', file_name) + '.csv'
+        train_path = os.path.join(preds_path, 'train', file_name) + '.csv'
+        model_path = os.path.join(models_path, file_name) + '.py'
+
+        train.to_csv(train_path, index=False)
+        test.to_csv(test_path, index=False)
+        shutil.copyfile(caller_path, model_path)
+
+    def _set_parameters(self):
+        self._init_default_parameters()
+
+        if 'name' in self.parameters:
+            self.model_name = self.parameters['name']
+        if 'fit' in self.parameters:
+            self.fit_name = self.parameters['fit']
+        if 'predict' in self.parameters:
+            self.predict_name = self.parameters['predict']
+        if 'pred_col' in self.parameters:
+            self.preds_col_num = self.parameters['pred_col']
+        if 'online_val' in self.parameters:
+            self.online_val_func = self.parameters['online_val']
+
+    def fit(self, train, cv, **kwargs):
+        if not hasattr(self.model, self.fit_name):
+            raise NotImplementedError("Fit method is not implemented")
+
+        if isinstance(self.model, GenericModel):
+            return getattr(self.model, self.fit_name)(train, cv)
+        else:
+            return getattr(self.model, self.fit_name)(*train, **kwargs)
+
+    def get_parameter(self, parameter):
+        return self.parameters[parameter]
+
+    def get_preds_col_number(self):
+        return self.preds_col_num
+
+    def predict(self, *args, **kwargs):
+        predict_result = getattr(self.model, self.predict_name)(*args, **kwargs)
+
+        if len(predict_result.shape) > 1:
+            return predict_result[:, self.get_preds_col_number()]
+
+        return predict_result
+
+    def preprocess_online(self, *args, **preprocessor_params):
+        for method in args:
+            self.online_preprocessors[method] = preprocessor_params
+
+    def run(self, data_loader, evaluator, fit_params, predict_params,
+          verbose=False):
+        train_preds = np.zeros((len(data_loader.get_train_ids()), 1))
+        test_preds = np.zeros((len(data_loader.get_test_ids()), 1))
+        data_generator = data_loader.get_split()
+
+        for fold, (tr_ind, cv_ind) in enumerate(data_generator):
+            X_tr, y_tr = data_loader.get_by_id('train', tr_ind)
+            X_cv, y_cv = data_loader.get_by_id('train', cv_ind)
+
+            X_tr, y_tr, X_cv = self._apply_preprocessors(X_tr, y_tr, X_cv)
+
+            if verbose: print("Start training the model '{}'... \n".format(
+                self.model_name))
+
+            if self.online_val_func:
+                fit_params[self.online_val_func] = [(X_cv, y_cv)]
+
+            self.fit((X_tr, y_tr), (X_cv, y_cv), **fit_params)
+
+            preds_cv = self.predict(X_cv, **predict_params)
+            preds_test_cur = self.predict(data_loader.test, **predict_params)
+
+            accuracy = evaluator(y_cv, preds_cv)
+            if verbose: print("Fold #{}: {}\n".format(fold + 1, accuracy))
+
+            train_preds[cv_ind, :] = preds_cv.reshape((-1, 1))
+            test_preds += preds_test_cur.reshape((-1, 1))
+
+        test_preds = test_preds / data_loader.get_n_splits()
+
+        overall = evaluator(data_loader.get_target(), train_preds)
+        if verbose: print("Overall accuracy: {}\n".format(overall))
+        return test_preds, train_preds, overall
+
+    def save(self, data_loader, results, caller_path, preds_path, models_path):
+        tr_idx = data_loader.get_train_ids()
+        test_idx = data_loader.get_test_ids()
+        id_col_name = data_loader.get_parameter('id')
+        target_col_name = data_loader.get_parameter('target')
+        preds_te, preds_tr, accuracy = results
+
+        train_data = np.hstack(
+            (
+                tr_idx.reshape((-1, 1)),
+                preds_tr.reshape((-1, 1))
+            ))
+
+        test_data = np.hstack(
+            (
+                test_idx.reshape((-1, 1)),
+                preds_te.reshape((-1, 1))
+            ))
+
+        train_submit = pd.DataFrame(train_data,
+            columns=[id_col_name, target_col_name])
+
+        test_submit = pd.DataFrame(test_data,
+            columns=[id_col_name, target_col_name])
+
+        self._save_files(train_submit, test_submit, accuracy,
+caller_path, preds_path, models_path)
